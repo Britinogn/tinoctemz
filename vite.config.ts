8b418b36006fff90d8706df7e4f93af54837a7a0
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-05-15 19:08:51 +0100

    feat: add vite-plugin-sitemap for automatic sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import sitemap from 'vite-plugin-sitemap'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+
+    sitemap({
+      hostname: 'https://tinoctemz.vercel.app',
+    }),
   ],
 
   resolve:{
